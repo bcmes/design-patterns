4c2aea840326d380f203206128cc8e5f00e61f52
bcmes <brunomeloesilva@gmail.com>
2023-05-20 20:17:45 -0400

    builds

---
+++ src/main/java/bcmes/com/github/dp3build/middle/car/Car.java
@@ -0,0 +1,9 @@
+package bcmes.com.github.dp3build.middle.car;
+
+public class Car {
+    private int seats;
+    private String engine;
+    private boolean gps;
+    //diversos oucolortros atributos...
+    //teria de gerar o boilerplate code...
+}
---
+++ src/main/java/bcmes/com/github/dp3build/middle/car/Manual.java
@@ -0,0 +1,9 @@
+package bcmes.com.github.dp3build.middle.car;
+
+public class Manual {
+    private String seats;
+    private String engine;
+    private String gps;
+    //diversos oucolortros atributos...
+    //teria de gerar o boilerplate code...
+}
